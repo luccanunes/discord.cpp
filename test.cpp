aca0199de351f0d1e6ef6db0b21797249a6adb25
Lucca Nunes <luccanunesgit@gmail.com>
2020-10-25 14:13:45 -0300

    :art: Change function pointers to std::function

--- test.cpp
+++ test.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include "dpp.h"
 
-dpp::Client client;
 
 int main()
 {
+	dpp::Client client;
 	std::string TOKEN = "NzY5MjIwMjEyODA4ODc2MDMz.X5L2Jg.To8C4xQog1OeDdeCTdT9b6lW86o";
 
-	client.onReady = []() {
-		std::cout << "hello, c++!" << std::endl;
-		std::cout << client.user.id << "\n\n";
+	client.onReady = [&]() {
+		std::cout << "ready to go boiiis\n";
+		std::cout << "hello, c++\n";
+		std::cout << client.user.id << "\n";
 	};
 
-	client.onMessage = [](dpp::Message message) {
-		std::cout << "its a message!!: " << message.content << std::endl;
-		if (message.content == "jaaj?") {
-			client.send("jooj.", message.channel_id);
-		}
+	client.onMessage = [&](dpp::Message message) {
+		std::cout << "NEW MESSAGE: " << message.content << std::endl;
 	};
+
 	client.run(TOKEN);
 }
