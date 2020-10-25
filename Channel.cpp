e5dd5d24baac17df5abd2f47b47345d6c192342f
Lucca Nunes <luccanunesgit@gmail.com>
2020-10-25 14:12:56 -0300

    :sparkles: Add Guild class and update Channel class

--- Channel.cpp
+++ Channel.cpp
@@ -1,4 +1,8 @@
+#include <iostream>
 #include "Channel.h"
+#include "Api.h"
+
+using json = nlohmann::json;
 
 namespace dpp {
 	void Channel::initialize(nlohmann::json props) {
@@ -14,5 +18,15 @@ namespace dpp {
 			guild_id = props["guild_id"];
 		if (!props["last_message_id"].is_null())
 			last_message_id = props["last_message_id"];
+
+		const std::string path = "/guilds/" + guild_id + "?with_counts=true";
+		json res = Api::get(path);
+		guild.initialize(res);
+	}
+	void Channel::send(std::string message) {
+		const std::string path = "/channels/" + id + "/messages";
+		json body = { {"content", message} };
+		json res = Api::post(path, cpr::Body{ body.dump() });
+		std::cout << res << std::endl;
 	}
 }
