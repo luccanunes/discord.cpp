096a08c1458aadf736fabce69f3800bf05cec7ea
Lucca Nunes <luccanunesgit@gmail.com>
2020-11-02 21:10:15 -0300

    update identation

--- src/Client.cpp
+++ src/Client.cpp
@@ -20,60 +20,59 @@ namespace dpp {
 		heartbeat_interval = -1;
 		connected = false;
 
-		webSocket.setOnMessageCallback([&](const ix::WebSocketMessagePtr& msg)
+		webSocket.setOnMessageCallback([&](const ix::WebSocketMessagePtr& msg) {
+			if (isTokenInvalid) return;
+			if (msg->type == ix::WebSocketMessageType::Message)
 			{
-				if (isTokenInvalid) return;
-				if (msg->type == ix::WebSocketMessageType::Message)
-				{
-					std::cout << "RESPONSE: " << msg->str << "\n\n";
-					json res = json::parse(msg->str);
-					if (res["s"].is_number())
-						lastS = res["s"];
-					int op = std::stoi(res["op"].dump());
-					switch (op) {
-					case 10:
-						heartbeat_interval = std::stoi(res["d"]["heartbeat_interval"].dump());
-						if (!connected) {
-							connected = true;
-							json heartbeat_p = {
-								{"op", 1},
-								{"d", nullptr}
-							};
-							std::cout << "SENDING HEARTBEAT: " << heartbeat_p.dump() << "\n\n";
-							webSocket.send(heartbeat_p.dump());
-						}
-						sendID();
-						break;
-					case 0:
-						std::string t = res["t"];
-						if (t == "READY") {
-							std::cout << "USER INFO: " << res["d"]["user"] << "\n\n";
-							user.initialize(res["d"]["user"]);
-							if (onReady)
-								onReady();
-						}
-						else if (t == "MESSAGE_CREATE") {
-							dpp::Message message(res["d"]);
-							if (onMessage)
-								onMessage(message);
-						}
-						break;
+				std::cout << "RESPONSE: " << msg->str << "\n\n";
+				json res = json::parse(msg->str);
+				if (res["s"].is_number())
+					lastS = res["s"];
+				int op = std::stoi(res["op"].dump());
+				switch (op) {
+				case 10:
+					heartbeat_interval = std::stoi(res["d"]["heartbeat_interval"].dump());
+					if (!connected) {
+						connected = true;
+						json heartbeat_p = {
+							{"op", 1},
+							{"d", nullptr}
+						};
+						std::cout << "SENDING HEARTBEAT: " << heartbeat_p.dump() << "\n\n";
+						webSocket.send(heartbeat_p.dump());
 					}
-				}
-				else if (msg->type == ix::WebSocketMessageType::Open)
-				{
-					std::cout << "Connection established" << "\n\n";
-				}
-				else if (msg->type == ix::WebSocketMessageType::Close) {
-					std::cout << "Disconnected" << std::endl;
-					std::cout << msg->closeInfo.code << std::endl;
-					switch (msg->closeInfo.code) {
-					case 4004:
-						std::cout << msg->closeInfo.reason << " Invalid token\n";
-						isTokenInvalid = true;
-						break;
+					sendID();
+					break;
+				case 0:
+					std::string t = res["t"];
+					if (t == "READY") {
+						std::cout << "USER INFO: " << res["d"]["user"] << "\n\n";
+						user.initialize(res["d"]["user"]);
+						if (onReady)
+							onReady();
 					}
+					else if (t == "MESSAGE_CREATE") {
+						dpp::Message message(res["d"]);
+						if (onMessage)
+							onMessage(message);
+					}
+					break;
 				}
+			}
+			else if (msg->type == ix::WebSocketMessageType::Open)
+			{
+				std::cout << "Connection established" << "\n\n";
+			}
+			else if (msg->type == ix::WebSocketMessageType::Close) {
+				std::cout << "Disconnected" << std::endl;
+				std::cout << msg->closeInfo.code << std::endl;
+				switch (msg->closeInfo.code) {
+				case 4004:
+					std::cout << msg->closeInfo.reason << " Invalid token\n";
+					isTokenInvalid = true;
+					break;
+				}
+			}
 			}
 		);
 
