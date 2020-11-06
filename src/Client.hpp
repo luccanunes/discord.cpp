91f97b5afe9e2e40eedb4454fdf0d8deffeb2684
Lucca Nunes <luccanunesgit@gmail.com>
2020-11-06 09:26:38 -0300

    :mute: Remove logging

--- src/Client.hpp
+++ src/Client.hpp
@@ -7,7 +7,6 @@
 #include <nlohmann/json.hpp>
 #include <functional>
 #include <stdexcept>
-#include <iostream>
 #include <string>
 #include "Message.hpp"
 #include "User.hpp"
@@ -31,7 +30,6 @@ namespace dpp {
 			webSocket.setOnMessageCallback([&](const ix::WebSocketMessagePtr& msg) {
 				if (isTokenInvalid) return;
 				if (msg->type == ix::WebSocketMessageType::Message) {
-					std::cout << "RESPONSE: " << msg->str << "\n\n";
 					nlohmann::json res = nlohmann::json::parse(msg->str);
 					if (res["s"].is_number())
 						lastS = res["s"];
@@ -45,7 +43,6 @@ namespace dpp {
 								{"op", 1},
 								{"d", nullptr}
 							};
-							std::cout << "SENDING HEARTBEAT: " << heartbeat_p.dump() << "\n\n";
 							webSocket.send(heartbeat_p.dump());
 						}
 						sendID();
@@ -53,7 +50,6 @@ namespace dpp {
 					case 0:
 						std::string t = res["t"];
 						if (t == "READY") {
-							std::cout << "USER INFO: " << res["d"]["user"] << "\n\n";
 							user.initialize(res["d"]["user"]);
 							if (onReady)
 								onReady();
@@ -67,14 +63,10 @@ namespace dpp {
 					}
 				}
 				else if (msg->type == ix::WebSocketMessageType::Open) {
-					std::cout << "Connection established" << "\n\n";
 				}
 				else if (msg->type == ix::WebSocketMessageType::Close) {
-					std::cout << "Disconnected" << std::endl;
-					std::cout << msg->closeInfo.code << std::endl;
 					switch (msg->closeInfo.code) {
 					case 4004:
-						std::cout << msg->closeInfo.reason << " Invalid token\n";
 						isTokenInvalid = true;
 						break;
 					}
@@ -95,7 +87,6 @@ namespace dpp {
 						{"op", 1},
 						{"d", lastS}
 					};
-					std::cout << "SENDING HEARTBEAT: " << payload.dump() << "\n\n";
 					webSocket.send(payload.dump());
 				}
 				std::this_thread::sleep_for(std::chrono::milliseconds(200));
@@ -104,7 +95,6 @@ namespace dpp {
 		inline void send(const std::string message, const std::string channel_id) const {
 			nlohmann::json body = { {"content", message} };
 			nlohmann::json res = Api::post("/channels/" + channel_id + "/messages", cpr::Body{ body.dump() });
-			std::cout << res << std::endl;
 		}
 
 		std::function<void(const Message& message)> onMessage;
@@ -125,7 +115,6 @@ namespace dpp {
 					{"token", token}, {"properties", props}, {"intents", 513}
 				}}
 			};
-			std::cout << "SENDING: " << id.dump() << "\n\n";
 			webSocket.send(id.dump());
 		}
 	};
